436f81353a760127d893ea899d412798ce98419c
liam <118787563+liamplecak@users.noreply.github.com>
2026-07-27 17:58:35 -0500

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -1,6 +1,7 @@
 import json
 import os
 import urllib.request
+from datetime import datetime, timezone
 
 API_KEY = os.environ["LASTFM_API_KEY"]
 USERNAME = os.environ["LASTFM_USERNAME"]
@@ -19,17 +20,35 @@ with urllib.request.urlopen(url) as response:
 
 track = data["recenttracks"]["track"][0]
 
-image = ""
-if len(track["image"]) > 0:
-    image = track["image"][-1]["#text"]
+images = {}
+
+for img in track["image"]:
+    images[img["size"]] = img["#text"]
+
+image = images.get("extralarge") or images.get("large") or ""
 
 music = {
     "track": track["name"],
     "artist": track["artist"]["#text"],
     "album": track["album"]["#text"],
+
     "cover": image,
+    "images": images,
+
     "playing": "@attr" in track and track["@attr"].get("nowplaying") == "true",
-    "timestamp": track.get("date", {}).get("#text", "")
+
+    "timestamp": track.get("date", {}).get("#text", ""),
+    "uts": track.get("date", {}).get("uts", ""),
+
+    "url": track["url"],
+
+    "artist_url": track["artist"].get("url", ""),
+
+    "album_url": track["album"].get("url", ""),
+
+    "loved": track.get("loved", "0") == "1"
+
+    "generated": datetime.now(timezone.utc).isoformat(),
 }
 
 with open("music.json", "w", encoding="utf-8") as f:
